dba0c48bfe776771f8f771c5dd33b5d7038a2413
elm-register <elmgameinfo@gmail.com>
2023-02-19 00:41:37 +0900

    Fix: ワークスペース内のBuildターゲットの修正をしました

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,8 +5,8 @@
 use core::panic::PanicInfo;
 
 #[no_mangle]
-pub extern "C" fn efi_main() -> ! {
-    common::assembly::hlt_forever()
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
 
 /// この関数はパニック時に呼ばれる
