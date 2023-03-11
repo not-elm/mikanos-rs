21fc8c666b5b49698744069fd906637865c6cbd3
elmga <elmgameinfo@gmai..com>
2023-03-11 13:52:59 +0900

    Fix: 退避させていた既存の処理を元に戻しました。

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,11 +5,13 @@
 #![test_runner(test_runner::my_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-
 use core::panic::PanicInfo;
 
 use common_lib::frame_buffer::FrameBufferConfig;
-use kernel_lib::gop::console::init_console;
+use common_lib::vector::Vector2D;
+use kernel_lib::error::KernelResult;
+use kernel_lib::gop::console::{draw_cursor, fill_rect_using_global, init_console};
+use kernel_lib::gop::pixel::pixel_color::PixelColor;
 use kernel_lib::println;
 
 #[cfg(test)]
@@ -19,11 +21,35 @@ mod test_runner;
 pub extern "sysv64" fn kernel_main(frame_buffer_config: FrameBufferConfig) -> () {
     init_console(frame_buffer_config);
     println!("hello!");
-    
+
     #[cfg(test)]
     test_main();
-    
+
+    fill_background(PixelColor::new(0x3E, 0x3E, 0x3E), &frame_buffer_config).unwrap();
+    fill_bottom_bar(PixelColor::new(0x00, 0x00, 0xFF), &frame_buffer_config).unwrap();
+
+    draw_cursor().unwrap();
+
     common_lib::assembly::hlt_forever();
+}
+
+fn fill_background(color: PixelColor, config: &FrameBufferConfig) -> KernelResult {
+    fill_rect_using_global(
+        Vector2D::new(0, 0),
+        Vector2D::new(config.horizontal_resolution, config.vertical_resolution),
+        color,
+    )
+}
+
+fn fill_bottom_bar(color: PixelColor, config: &FrameBufferConfig) -> KernelResult {
+    let v = config.vertical_resolution;
+    let h = config.horizontal_resolution;
+    fill_rect_using_global(Vector2D::new(0, v - 50), Vector2D::new(h, v), color)?;
+    fill_rect_using_global(
+        Vector2D::new(0, v - 50),
+        Vector2D::new(50, v),
+        PixelColor::new(0x33, 0x33, 0xAA),
+    )
 }
 
 /// この関数はパニック時に呼ばれる
@@ -34,7 +60,6 @@ fn panic(info: &PanicInfo) -> ! {
     common_lib::assembly::hlt_forever();
 }
 
-
 #[panic_handler]
 #[cfg(test)]
 fn panic(info: &PanicInfo) -> ! {
@@ -42,4 +67,3 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{:?}", info);
     common_lib::assembly::hlt_forever();
 }
-
