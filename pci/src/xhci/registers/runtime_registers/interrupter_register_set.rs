8c47fa09d8bec03133f5b7688585d7e3eb713791
elmga <elmgameinfo@gmai..com>
2023-03-19 22:08:06 +0900

    Add: コメント追加

--- pci/src/xhci/registers/runtime_registers/interrupter_register_set.rs
+++ pci/src/xhci/registers/runtime_registers/interrupter_register_set.rs
@@ -1,6 +1,21 @@
 /// IRO
 ///
-/// 
+/// # Offset
 ///
-/// Offset: Base(Primary) 0x20 Bytes
+/// Base(Primary) 0x20 Bytes
+///
+/// # Description
+/// このレジスタはRunTimeRegistersの中に最大1024個配置でき、
+/// 先頭の要素はPrimaryInterrupterと呼ばれます。
+///
+/// # Notes
+///
+/// * PrimaryInterrupterの中のレジスタ群はRunStopが1になるまえに初期化する必要があります。
+///
+/// * SecondaryInterrupters(恐らくPrimary以外を指す)はRunStopが1になった後でも初期化できますが、
+/// 自身を対象にしたイベントが発行される前に初期化する必要があります。
+///
+/// [Xhci Document] : 424 Page
+///
+/// [Xhci Document]: https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf
 pub struct InterrupterRegisterSet {}
