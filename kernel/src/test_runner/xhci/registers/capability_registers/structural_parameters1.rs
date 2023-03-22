0acad658199af18436fdf5ae8dcd5436382121b8
elm_register <elmgameinfo@gmail.com>
2023-03-22 10:49:37 +0900

    Add: MaxPortsの追加

--- kernel/src/test_runner/xhci/registers/capability_registers/structural_parameters1.rs
+++ kernel/src/test_runner/xhci/registers/capability_registers/structural_parameters1.rs
@@ -3,7 +3,7 @@ use pci::xhci::registers::capability_registers::structural_parameters1::Structur
 use crate::{hcs1_offset, serial_println};
 
 #[test_case]
-fn it_fetch_mmio_base_addr() {
+fn it_read_max_ports() {
     let ports = StructuralParameters1::new(hcs1_offset()).max_ports();
     serial_println!("max_ports={}", ports);
     assert!(0 < ports)
