80fd44186a736e5054117e2163cc20f75efccf6a
elm_register <elmgameinfo@gmail.com>
2023-06-28 10:56:58 +0900

    DELETE: Unused Struct

--- kernel-lib/src/timer.rs
+++ kernel-lib/src/timer.rs
@@ -3,7 +3,6 @@ use core::sync::atomic::Ordering;
 use crate::timer::handler::manager::TimeHandleManager;
 
 pub mod apic;
-pub mod timer_manager;
 pub mod handler;
 
 pub static TIME_HANDLE_MANAGER: TimeHandleManager = TimeHandleManager::new();
